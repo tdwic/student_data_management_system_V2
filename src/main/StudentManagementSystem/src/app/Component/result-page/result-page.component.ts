c17010dc4831eb5500c24740ad7dae51ab4bfbaf
tdwic <36078972+tdwic@users.noreply.github.com>
2020-08-12 00:33:41 +0530

    result page done

--- src/main/StudentManagementSystem/src/app/Component/result-page/result-page.component.ts
+++ src/main/StudentManagementSystem/src/app/Component/result-page/result-page.component.ts
@@ -1,4 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import {MatTableDataSource} from "@angular/material/table";
+import {CommonService} from "../../Services/CommonService/common.service";
+
+class StudentMarks {
+  recordID: string;
+  studentID: string;
+  studentName: string;
+  firstTermMarks: string;
+  secondTermMarks: string;
+  thirdTermMarks: string;
+  firstTermNote: string;
+  secondTermNote: string;
+  thirdTermNote: string;
+}
 
 @Component({
   selector: 'app-result-page',
@@ -7,9 +21,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResultPageComponent implements OnInit {
 
-  constructor() { }
+  studentMarksList:StudentMarks[];
+  displayedColumns:string[] = ['studentID','firstTermMarks','secondTermMarks','thirdTermMarks','firstTermNote','secondTermNote','thirdTermNote'];
+  dataSource = new MatTableDataSource(this.studentMarksList);
+
+  constructor(private _commonService : CommonService) { }
 
   ngOnInit(): void {
+    this.populateTable();
+  }
+
+  populateTable(){
+
+    let studentID = localStorage.getItem('studentID');
+
+    this._commonService.getStudentMarks(studentID).subscribe( res => {
+      this.studentMarksList = res;
+      this.dataSource = new MatTableDataSource<StudentMarks>(this.studentMarksList);
+    })
   }
 
 }
